2ea2be84828bebe8f67ceef6c204ada7c233a6b9
Javier <javier.manzano22@gmail.com>
2019-05-24 16:35:06 +0200

    chore(): modified

--- handler.js
+++ handler.js
@@ -2,9 +2,10 @@ const restClient = require('./rest-client');
 
 const COMMAND_TEXT_MAPPER = {
   help: () => `Hi! :wave: \n I'm the most fair & easy poll creator \n 100% secure. No manipulation allowed`,
-  create: () => `New poll created. Let's vote: `,
-  status: () => `Here are the results:`,
-  list: () => `All the available polls`,
+  create: () => `*New poll created. Let's vote:* `,
+  status: () => `*Here are the results:* `,
+  list: () => `*All the available polls*`,
+  close: () => `*Poll Closed!*`,
 };
 
 const COMMAND_HANDLERS = {
@@ -12,7 +13,11 @@ const COMMAND_HANDLERS = {
   create: async ({ arguments }) => {
     console.log('--> requesting poll URL with options', { arguments });
     try {
-      const response = await restClient.post({ url: 'create', body: {options: arguments} })
+      const response = await restClient
+        .post({
+          url: 'create',
+          body: { name: arguments[0], options: arguments.slice(1) }
+        });
       console.log({ response });
       return response.url;
     } catch(err) {
@@ -35,12 +40,18 @@ const COMMAND_HANDLERS = {
   list: async () => () => {
     console.log('--> requesting polls list');
   },
+  close: async ({ arguments }) => {
+    console.log('--> requesting poll status', arguments);
+    const id = arguments[0];
+    const response = await restClient.post({ url: `${id}/close` })
+    console.log({ response });
+  },
 };
 
 const AVAILABLE_COMMANDS = Object.keys(COMMAND_TEXT_MAPPER);
 
 const getArguments = (commandArguments) => (
-  commandArguments.map(commandOption => commandOption.replace(/"/g,''))
+  commandArguments.map(commandOption => commandOption.replace(/"/g,'')).filter(c => (c !== ' ' && c !== ''))
 );
 
 const processResponseDecorator = ({ torralbotCommand, arguments }) => ({ text }) => ({
@@ -50,12 +61,18 @@ const processResponseDecorator = ({ torralbotCommand, arguments }) => ({ text })
 });
 
 const process = ({ command }) => {
-  const splittedCommand = command.split(' ');
-  const torralbotCommand = splittedCommand[0];
+  let splittedCommand = command.split(" ");
+  if (command.indexOf(`"`) !== -1) {
+    splittedCommand = command.split(`"`);
+  }
+
+  const torralbotCommand = splittedCommand[0].trim();
   let arguments = [];
   if (splittedCommand.length > 1) {
     arguments = getArguments(splittedCommand.slice(1));
   }
+  console.log({ arguments0: arguments});
+  console.log({ torralbotCommand });
   const responseGenerator = processResponseDecorator({ arguments, torralbotCommand });
   if (AVAILABLE_COMMANDS.indexOf(torralbotCommand)) {
     return responseGenerator({
